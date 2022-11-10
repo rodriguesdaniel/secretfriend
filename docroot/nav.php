9736eba68bcf826fb305bbf821b5904620eb3ce7
Daniel Rodrigues <daniel.ydeia@gmail.com>
2022-11-10 18:13:34 -0300

    Nav bar admin mode only

--- docroot/nav.php
+++ docroot/nav.php
@@ -2,7 +2,13 @@
     <h3 class="float-md-start mb-0">Amigo Secreto</h3>
     <nav class="nav nav-masthead justify-content-center float-md-end">
     <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="./">Home</a>
-    <a class="nav-link fw-bold py-1 px-0" href="admin.php">Adicionar</a>
-    <a class="nav-link fw-bold py-1 px-0" href="sortear.php">Sortear</a>
+    <a class="nav-link fw-bold py-1 px-0" href="admin.php">Participar</a>
+    <?php
+        @$mode = addslashes($_GET['mode']);
+
+        if($mode == 'admin') {
+            echo '<a class="nav-link fw-bold py-1 px-0" href="sortear.php">Sortear</a>';
+        }
+    ?>
     </nav>
 </div>
